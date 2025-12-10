6fe6844e668a728bf6161f39879897e1151ace3c
Muhammad Salem - EightySixCode <salem@eightysixcode.ae>
2025-12-10 18:52:44 +0400

    Update import for create_retrieval_chain
    On v1 langchain.chains has moved to langchain_classic.chains

--- src/rag.py
+++ src/rag.py
@@ -6,7 +6,7 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain.chains import create_retrieval_chain
+from langchain_classic.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_history_aware_retriever
 from langchain_core.prompts import MessagesPlaceholder
